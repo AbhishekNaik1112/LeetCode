8ce3de61bfa34a6ce5a2931968e21ef7aec73a27
Abhishek Naik <142667674+AbhishekNaik1112@users.noreply.github.com>
2024-06-19 14:56:55 +0530

    Time: 0 ms (100%), Space: 11 MB (32.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int x = m - 1;
         int y = n - 1;
-        int z = m + n - 1;
+        int z = m + n - 1;  
 
         while (y >= 0) {
             if (x >= 0 && nums1[x] >= nums2[y]) {
@@ -15,5 +15,3 @@ public:
     }
 };
 
-// [3.5.0.0.0]
-//[1.2.3.6.5]

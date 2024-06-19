3d761a74079713835ed258e665e0cb250ae150fa
Abhishek Naik <142667674+AbhishekNaik1112@users.noreply.github.com>
2024-06-19 14:58:13 +0530

    Time: 5 ms (28.15%), Space: 10.9 MB (71.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int x = m - 1;
+        int x = m - 1;  
         int y = n - 1;
-        int z = m + n - 1;  
+        int z = m + n - 1; 
 
         while (y >= 0) {
             if (x >= 0 && nums1[x] >= nums2[y]) {

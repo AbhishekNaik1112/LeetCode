eebfd5a7d3def2f9abec78be40cc9831d76aa813
Abhishek Naik <142667674+AbhishekNaik1112@users.noreply.github.com>
2024-06-26 12:45:13 +0530

    Time: 19 ms (74.41%), Space: 20.5 MB (64.43%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        // using greedy
+
+        // sort both arrays
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
+
+        // size of each array
         int gs = g.size();
         int ss = s.size();
+
+        // make a count
         int ct = 0;
+
+        // initial index
         int i = 0;
         int j = 0;
+
+        // run loop till one reaches the end
         while (i < gs && j < ss) {
+            // to satisfy curr child
             if (s[j] >= g[i]) {
-                ct++;
-                i++;
+                ct++; // increment
+                i++;  // next child
             }
-            j++;
+            j++; // next cookie
         }
-        return ct;
+        return ct;  //return count
     }
 };

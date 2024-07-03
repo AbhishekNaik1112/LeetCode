59c7397311e7cb2c809270fb64686493aca89e4f
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-03 08:34:07 +0530

    Time: 0 ms (100%), Space: 10.6 MB (40.8%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -5,12 +5,8 @@ public:
         for (int x : arr) {
             if (x % 2 != 0) {
                 ct++;
-                if (ct == 3) {
-                    return true;
-                }
-            } else {
-                ct = 0;
-            }
+                if (ct == 3) return true;
+            } else ct = 0;
         }
         return false;
     }

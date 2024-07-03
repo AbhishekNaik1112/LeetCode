d4af2a1a5acebe097abc7f017eb08633d5d59c55
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-03 08:33:41 +0530

    Time: 3 ms (57.96%), Space: 10.4 MB (79.82%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int ct = 0;
+        for (int x : arr) {
+            if (x % 2 != 0) {
+                ct++;
+                if (ct == 3) {
+                    return true;
+                }
+            } else {
+                ct = 0;
+            }
+        }
+        return false;
+    }
+};

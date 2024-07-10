2d317fb988170f1f422781d4651539ae4d1a3d21
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-10 16:53:23 +0530

    Time: 257 ms (5.34%), Space: 15 MB (73.1%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.cpp
+++ 0868-push-dominoes/0868-push-dominoes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string pushDominoes(string dominoes) {
         string temp;
-         
+
         while (dominoes != temp) {
             temp = dominoes;
             
@@ -10,23 +10,19 @@ public:
             for (size_t pos = 0; (pos = dominoes.find("R.L", pos)) != string::npos; pos += 3) {
                 dominoes.replace(pos, 3, "xxx");
             }
-
             // Step 2: Replace "R." with "RR"
             for (size_t pos = 0; (pos = dominoes.find("R.", pos)) != string::npos; pos += 2) {
                 dominoes.replace(pos, 2, "RR");
             }
-
             // Step 2: Replace ".L" with "LL"
             for (size_t pos = 0; (pos = dominoes.find(".L", pos)) != string::npos; pos += 2) {
                 dominoes.replace(pos, 2, "LL");
             }
         }
-        
         // Step 3: Replace "xxx" with "R.L"
         for (size_t pos = 0; (pos = dominoes.find("xxx", pos)) != string::npos; pos += 3) {
             dominoes.replace(pos, 3, "R.L");
-        }
-        
+        }  
         return dominoes;
     }
 };

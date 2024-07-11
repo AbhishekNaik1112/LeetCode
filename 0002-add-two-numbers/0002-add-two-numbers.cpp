9386d6a2437dc907a0e2e209a23ea782e49410b4
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-11 16:14:00 +0530

    Time: 25 ms (35.97%), Space: 78.2 MB (5.46%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,13 +12,19 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL && l2 == NULL) return NULL; //if both ll empty return null
-        else if (l1 == NULL) return l2; // if l1 null return l2
-        else if (l2 == NULL) return l1; // if l2 null return l1
-        int a = l1->val + l2->val; // add vals
+        if (l1 == NULL && l2 == NULL)
+            return NULL; // if both ll empty return null
+        else if (l1 == NULL)
+            return l2; // if l1 null return l2
+        else if (l2 == NULL)
+            return l1;                      // if l2 null return l1
+        int a = l1->val + l2->val;          // add vals
         ListNode* p = new ListNode(a % 10); // create node to manage carryovers
-        p->next = addTwoNumbers(l1->next, l2->next); //recursion to manage next nodes
-        if (a >= 10) p->next = addTwoNumbers(p->next, new ListNode(1)); //if a > 10  carryover to next node
-        return p; //return node which you created
+        p->next =
+            addTwoNumbers(l1->next, l2->next); // recursion to manage next nodes
+        if (a >= 10)
+            p->next = addTwoNumbers(
+                p->next, new ListNode(1)); // if a > 10  carryover to next node
+        return p;                          // return node which you created
     }
 };

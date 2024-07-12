6b2f4918bd1b10c752c4fc151a4423d7b5aec29c
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-12 11:35:11 +0530

    Time: 5 ms (40.89%), Space: 7.6 MB (62.26%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,59 +1,35 @@
 class Solution {
 public:
-    // Function to check if it's safe to place a queen at board[r][c]
-    bool check(vector<string>& board, int r, int c) {
-        // same col
-        for (int i = r; i >= 0; --i) {
-            if (board[i][c] == 'Q') {
+    bool check(vector<string>& board, int r, int c) { //check to see if you can place q
+        for (int i = 0; i < r; ++i)
+            if (board[i][c] == 'Q' ||// col
+                (c - r + i >= 0 && board[i][c - r + i] == 'Q') ||   //upp l
+                (c + r - i < board.size() && board[i][c + r - i] == 'Q')) //upp r
                 return false;
-            }
-        }
-
-        // upp l dia
-        for (int i = r, j = c; i >= 0 && j >= 0; --i, --j) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        // upp r dia
-        for (int i = r, j = c; i >= 0 && j < board.size(); --i, ++j) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        // no queen ret 1
         return true;
     }
+
     void search(vector<string>& board, int r, int& ans) {
-        // ans++ if all q
-        if (r == board.size()) {
+        if (r == board.size()) { //if all q alr there ans++
             ans++;
             return;
         }
 
-        // place q in each col in curr row
         for (int i = 0; i < board.size(); ++i) {
             if (check(board, r, i)) {
-                board[r][i] = 'Q';
-
+                board[r][i] = 'Q';//place q
                 search(board, r + 1, ans);
-
-                // Bktk mark
-                board[r][i] = '.';
+                board[r][i] = '.'; //bktk and change it to . again
             }
         }
     }
+
     int totalNQueens(int n) {
         if (n <= 0)
             return 0;
         int ans = 0;
-        // board with .
-        vector<string> board(n, string(n, '.'));
-
+        vector<string> board(n, string(n, '.')); //make board with all .
         search(board, 0, ans);
-
         return ans;
     }
 };

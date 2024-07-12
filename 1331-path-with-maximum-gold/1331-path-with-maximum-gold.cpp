9c82d712e1f04fc97510f655d96ac01b2b5b434b
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-12 11:37:18 +0530

    Time: 231 ms (23.36%), Space: 10.5 MB (9.46%) - LeetHub

---
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int maxGold = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] > 0) { //if cell has gold use rec to find
+                    maxGold = max(maxGold, dfs(grid, i, j));
+                }
+            }
+        }
+        return maxGold;
+    }
+
+private:
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) { //OoB and 0 gold
+            return 0;
+        }
+
+        int originalValue = grid[i][j];
+        grid[i][j] = 0; //marking cel
+
+      //go all dirs
+        int up = dfs(grid, i - 1, j);
+        int down = dfs(grid, i + 1, j);
+        int left = dfs(grid, i, j - 1);
+        int right = dfs(grid, i, j + 1);
+
+        //change to original value
+        grid[i][j] = originalValue;
+
+        // return maxgold
+        return originalValue + max({up, down, left, right});
+    }
+};

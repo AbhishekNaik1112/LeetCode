98a430bc29acd3c380f4601d6eb689d5b164014d
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-14 09:42:34 +0530

    Time: 26 ms (51.48%), Space: 13.5 MB (8.23%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,19 +1,21 @@
 class Solution {
+
 private:
     int maxChars(string s, char ch, int k) {
         int ans = 0;
         int i = 0;
         int ct = 0;
+
         for (int j = 0; j < s.size(); j++) {
             if (s[j] != ch)
                 ct++;
-
             while (ct > k) {
                 if (s[i] != ch)
                     ct--;
                 i++;
             }
-            ans = max(ans, j - i + 1);
+
+            ans = max(ans, (j - i + 1));
         }
         return ans;
     }

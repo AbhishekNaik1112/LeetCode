1ac143d00225152f83630b9963451db84e352f5d
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-16 17:08:18 +0530

    Time: 3 ms (38.29%), Space: 8.1 MB (79.41%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         unordered_map<char, string> phone = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
+            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
             {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"},
         };
 
         vector<string> ans;
+
         if (digits.empty()) {
-            return ans; 
+            return ans;
         }
-        ans.push_back(""); 
+        ans.push_back("");
 
         for (char digit : digits) {
             const string& letters = phone[digit];

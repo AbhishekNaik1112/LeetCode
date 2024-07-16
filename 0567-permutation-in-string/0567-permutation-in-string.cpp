8657dbc6229dd5e2c9b1909fb1d4bb5450c8c978
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-16 15:31:26 +0530

    Time: 5 ms (70.59%), Space: 8.7 MB (45.02%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         vector<int> curr(26), tracker(26);
+
         for (char ch : s1)
             tracker[ch - 'a']++;
+
         for (int i = 0; i < s2.size(); i++) {
             curr[s2[i] - 'a']++;
-            if (i >= s1.size()) {
+
+            if (i >= s1.size())
                 curr[s2[i - s1.size()] - 'a']--;
-            }
+
             if (tracker == curr)
                 return true;
         }

fc362f9e21abf33b5c6069e38cafe2eededa1f25
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-16 13:45:45 +0530

    Time: 4 ms (73.63%), Space: 13.7 MB (5.41%) - LeetHub

---
+++ 0985-bag-of-tokens/0985-bag-of-tokens.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end()); // sort tokens
+        int res = 0, points = 0;
+        int i = 0, j = tokens.size() - 1; // i=left, j=right
+        
+        while (i <= j) { // left<=right
+            if (power >= tokens[i]) { // enough power for playing?
+                power -= tokens[i++]; // reduce power, move left pointer
+                res = max(res, ++points); // increase points, update res
+            } else if (points > 0) { // not enough power but have points?
+                points--; // give up points
+                power += tokens[j--]; // gain power, move right pointer
+            } else {
+                break; // cannot play anymore
+            }
+        }
+        return res; // return max score
+    }
+};

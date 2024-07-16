2b869eb141a2dfd86e160221d8ed077afdd531d9
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-16 15:21:47 +0530

    Time: 7 ms (65.58%), Space: 8.8 MB (43.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> curr(26), track(26);
-        for (char c : s1)
-            track[c - 'a']++;
+        vector<int> curr(26), tracker(26);
+        for (char ch : s1)
+            tracker[ch - 'a']++;
         for (int i = 0; i < s2.size(); i++) {
             curr[s2[i] - 'a']++;
-            if (i >= s1.size())
+            if (i >= s1.size()) {
                 curr[s2[i - s1.size()] - 'a']--;
-            if (track == curr)
+            }
+            if (tracker == curr)
                 return true;
         }
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool checkInclusion(string s1, string s2) {
+//         vector<int> curr(26), tracker(26);
+//         for (char c : s1)
+//             tracker[c - 'a']++;
+//         for (int i = 0; i < s2.size(); i++) {
+//             curr[s2[i] - 'a']++;
+//             if (i >= s1.size())
+//                 curr[s2[i - s1.size()] - 'a']--;
+//             if (tracker == curr)
+//                 return true;
+//         }
+//         return false;
+//     }
+// };

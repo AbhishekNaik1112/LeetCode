455ca93c2c4c9ea8c6faa70041b565bad8b452db
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-18 16:46:15 +0530

    Time: 3 ms (96.84%), Space: 15.7 MB (20.93%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        unordered_map<string, function<int (int, int) > > op = {
-            { "+" , [] (int a, int b) { return a + b; } },
-            { "-" , [] (int a, int b) { return a - b; } },
-            { "*" , [] (int a, int b) { return a * b; } },
-            { "/" , [] (int a, int b) { return a / b; } }
-        };
-        stack<int> stack;
+        unordered_map<string, function<int(int, int)>> op = {
+            {"+", [](int a, int b) { return a + b; }},
+            {"-", [](int a, int b) { return a - b; }},
+            {"*", [](int a, int b) { return a * b; }},
+            {"/", [](int a, int b) { return a / b; }}};
+        stack<int> st;
+
         for (string& s : tokens) {
             if (!op.count(s)) {
-                stack.push(stoi(s));
+                st.push(stoi(s));
             } else {
-                int n1 = stack.top();
-                stack.pop();
-                int n2 = stack.top();
-                stack.pop();
-                stack.push(op[s](n2, n1));
+                int n1 = st.top();
+                st.pop();
+                int n2 = st.top();
+                st.pop();
+                st.push(op[s](n2, n1));
             }
         }
-        return stack.top();
+        return st.top();
     }
 };

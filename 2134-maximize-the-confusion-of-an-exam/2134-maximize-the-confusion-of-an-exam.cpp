732e7b465bda30ab6c1cd84c919624a507d4a4aa
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-18 12:15:28 +0530

    Time: 27 ms (46.18%), Space: 13.5 MB (8.31%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,23 +1,25 @@
 class Solution {
-    private:
-    int maxChars(string s,int k,char ch){
+private:
+    int maxChars(string s, int k, char ch) {
         int ct = 0;
-        int i=0;
+        int i = 0;
         int ans = 0;
 
-        for(int j=0;j<s.size();j++){
-            if(s[j]!=ch) ct++;
-            while(ct>k){
-                if(s[i]!=ch)
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] != ch)
+                ct++;
+            while (ct > k) {
+                if (s[i] != ch)
                     ct--;
                 i++;
             }
-            ans = max(ans,(j-i+1));
+            ans = max(ans, (j - i + 1));
         }
         return ans;
     }
+
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        return max(maxChars(answerKey,k,'T'),maxChars(answerKey,k,'F'));
+        return max(maxChars(answerKey, k, 'T'), maxChars(answerKey, k, 'F'));
     }
 };

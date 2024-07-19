d02d27de0d1f8f3328bdfe99808553aa3b6d4e78
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-19 17:36:11 +0530

    Time: 20 ms (64.61%), Space: 78.4 MB (5.47%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -20,11 +20,9 @@ public:
             return l1;                      // if l2 null return l1
         int a = l1->val + l2->val;          // add vals
         ListNode* p = new ListNode(a % 10); // create node to manage carryovers
-        p->next =
-            addTwoNumbers(l1->next, l2->next); // recursion to manage next nodes
+        p->next =addTwoNumbers(l1->next, l2->next); // recursion to manage next nodes
         if (a >= 10)
-            p->next = addTwoNumbers(
-                p->next, new ListNode(1)); // if a > 10  carryover to next node
+            p->next = addTwoNumbers(p->next, new ListNode(1)); // if a > 10  carryover to next node
         return p;                          // return node which you created
     }
 };

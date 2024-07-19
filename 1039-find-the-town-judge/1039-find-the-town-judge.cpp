7ff9c865f4572eb5014f10ffb9247cbbf865eea3
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-19 15:35:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> ct(n+1,0);
-        for(auto& it : trust){
-            ct[it[0]]--;
-            ct[it[1]]++;
-for(int i=1;i<=trust.size();i++){
-    if(ct[i]==1){
-        return i;
-    }
-}
-
-        }
+        vector<int> ct(n + 1, 0);
+        for (auto& it : trust)
+            ct[it[0]]--, ct[it[1]]++;
+        for (int i = 1; i <= n; i++)
+            if (ct[i] == 1)
+                return i;
         return -1;
     }
 };

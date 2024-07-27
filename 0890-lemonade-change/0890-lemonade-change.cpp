30debaa6ea595407a89955929bafa4a18c50be5c
Abhishek Naik <abhisheknaik1112@gmail.com>
2024-07-27 15:58:08 +0530

    Time: 68 ms (74.62%), Space: 86 MB (17.99%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -10,7 +10,7 @@ public:
                 if(five>0) five--,ten++;
                 else return false;
             }else if(bill==20){
-                if(five>1 && ten >1) five--,ten--;
+                if(five>0 && ten >0) five--,ten--;
                 else if(five>2) five-=3;
                 else return false;
             }
